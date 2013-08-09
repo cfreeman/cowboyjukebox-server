e566d0ea27f2bdc72982b142eb8631ee69f4484a
clinton <clinton.freeman@nicta.com.au>
2013-08-09 18:01:16 +1000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CowboyjukeboxServer::Application.config.secret_key_base = '5dc1143bd6492680db691fba09776584367510a8bc217f554a310d4468bd9710e21b5b1864231267d3db58090c56ebb94ecba1851b25a1f5bec8fe8364540fa1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CowboyjukeboxServer::Application.config.session_store :cookie_store, key: '_cowboyjukebox-server_session'
